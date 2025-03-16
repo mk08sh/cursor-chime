5d902898f0ee280697bde891ac7322149b26ed52
Mandad Tabrizi <mandad.tabrizi@gmail.com>
2025-03-16 00:03:10 -0700

    Fix build issues and add critters dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: true,
-  },
   typescript: {
     // We handle AudioContext errors in the code
     ignoreBuildErrors: true
